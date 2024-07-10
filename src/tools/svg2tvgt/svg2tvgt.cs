TinyVG/sdk

commit b8e82bc8eb1ba516aed1ac3445568afed6558e45

tools/svg2tvgt: handle scientific notation in float
This updates the svg2tvgt to handle scientific notation in
floating-point values, which can exist, apparently. ;)

src/tools/svg2tvgt/svg2tvgt.cs
@@ -2192,20 +2192,20 @@ public class SvgPathParser
     {
       while (true)
       {
-        c = PeekAny("0123456789.");
+        c = PeekAny("0123456789.eE+-");
         if (c == null)
           return ParseFloat(begin.Slice());
-        AcceptChar("0123456789.");
+        AcceptChar("0123456789.eE+-");
         if (c.Value == '.')
           break;
       }
     }
     while (true)
     {
-      c = PeekAny("0123456789");
+      c = PeekAny("0123456789eE+-");
       if (c == null)
         return ParseFloat(begin.Slice());
-      AcceptChar("0123456789");
+      AcceptChar("0123456789eE+-");
     }
   }
 

commit 00888cc1e536ccc8d050c85a439f566c6bacd7e5

svg2tvgt: fix ParseNumberGeneric
This fixes a regression in recent commit b8e82bc8eb1ba516aed1ac3445568afed6558e45.

That commit added the characters `eE+-` to the list of accepted characters
within the middle of a float string.  However, the `+-` characters are only
valid immediately after the `eE` characters.  This broke SVGS that don't
use whitespace between floats with negative numbers.  Consider the
following snippet from a real SVG:

```
<path d="M0 9.913V.487c0-.435.514-.651.812-.34l6.551 ...
```

With the commit referenced above, the string `0-.435` gets interpreted as a
single float value which fails to parse as a float.  I've fixed this
by splitting the function to parse floats into 3 parts:

1) before the decimal point

    char first = AcceptChar("0123456789." + (allow_sign ? "+-" : ""));
    ScanWhile(char.IsDigit);

2) after the decimal point

    if (first != '.' && ScanOne(".")) {
        ScanWhile(char.IsDigit);
    }

3) the exponent

    if (ScanOne("eE")) {
        ScanOne("+-");
        ScanWhile(char.IsDigit);
    }

This fixes the parser so that it only includes the `+-` characters if they
properly appear immediately after the `eE` characters.

src/tools/svg2tvgt/svg2tvgt.cs
@@ -2190,29 +2190,40 @@ public class SvgPathParser
     return ParseNumberGeneric(false);
   }
 
+  bool ScanOne(string s)
+  {
+    if (char_offset < path_text.Length && s.Contains(path_text[char_offset])) {
+      char_offset++;
+      return true;
+    }
+    return false;
+  }
+
+  delegate bool IncludeCallback(char c);
+
+  void ScanWhile(IncludeCallback cb)
+  {
+    while (char_offset < path_text.Length) {
+      if (!cb(path_text[char_offset]))
+        return;
+      char_offset++;
+    }
+  }
+
   float ParseNumberGeneric(bool allow_sign)
   {
     var begin = Save();
-    char? c = AcceptChar("0123456789." + (allow_sign ? "+-" : ""));
-    if (c != '.')
-    {
-      while (true)
-      {
-        c = PeekAny("0123456789.eE+-");
-        if (c == null)
-          return ParseFloat(begin.Slice());
-        AcceptChar("0123456789.eE+-");
-        if (c.Value == '.')
-          break;
-      }
+
+    char first = AcceptChar("0123456789." + (allow_sign ? "+-" : ""));
+    ScanWhile(char.IsDigit);
+    if (first != '.' && ScanOne(".")) {
+        ScanWhile(char.IsDigit);
     }
-    while (true)
-    {
-      c = PeekAny("0123456789eE+-");
-      if (c == null)
-        return ParseFloat(begin.Slice());
-      AcceptChar("0123456789eE+-");
+    if (ScanOne("eE")) {
+        ScanOne("+-");
+        ScanWhile(char.IsDigit);
     }
+    return ParseFloat(begin.Slice());
   }
 
   static float ParseFloat(string str)